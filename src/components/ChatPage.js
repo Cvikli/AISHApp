ff18d5e93262d34554574bf4935b5fb3ac0bc01b
Cvikli <havlikmarcell@gmail.com>
2024-08-19 12:02:39 +0200

    ChatComponent import path correction

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import ChatComponent from '../Chat';
+import ChatComponent from './ChatComponent';
 import { useAppContext } from '../contexts/AppContext';
 
 function ChatPage() {
